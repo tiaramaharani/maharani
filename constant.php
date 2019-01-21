a01062c872bb10cbeca8a010d018ba9304457c66
tiaramaharani <tiaramaharani13.tm@gmail.com>
2019-01-21 12:42:49 +0700

    commit pertama

---
+++ constant.php
@@ -0,0 +1,35 @@
+<?php 
+
+// define('NAMA', 'Tiara Maharani');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 17;
+// echo UMUR;
+
+// class Coba {
+// 	const NAMA = 'Tiara';
+// }
+
+// echo Coba::NAMA;
+
+
+
+// echo __FILE__;
+
+
+// function coba() {
+// 	return __FUNCTION__;
+// }
+
+// echo coba();
+
+class Coba {
+	public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
+
+ ?>
